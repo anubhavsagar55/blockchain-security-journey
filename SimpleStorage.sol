370fb7191f3c7eedc1950e251d0980040dc3e815
Anubhav Sagar <anubhavofficial55@gmail.com>
2026-06-15 14:26:26 +0530

    Add SimpleStorage contract - Solidity basic variable types
    Learning Solidity fundamentals. This contract covers basic 
    variable types: bool, uint, int, string, address, and bytes32.

---
+++ SimpleStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+
+    //Basic Variable Types :- boolean,unit,int,string,address,byte
+    bool hasfavoritenumber = true;
+    uint favoritenumber = 88;
+    int256 favoritenumber2 = -23;
+    string favoritenumberintext = "eighty-eight" ;
+    address myAddress = address (0x4341bC993409BC7cAF31F6B0e74b69dEfBdDf6a3) ;
+    bytes32 favoritebytes32 = "cat";
+} 
